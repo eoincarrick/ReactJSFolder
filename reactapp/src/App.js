09efd369666b08a91b94afd2086a8087fe1be65a
Eoin Carrick <22carri99@gmail.com>
2022-02-19 21:06:08 +0000

    code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,48 @@
+import { firstName, lastName } from "./components/index.js";
+import { useState } from "react";
 import "./css/App.css";
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [dick, setDick] = useState([
+    {
+      id: Math.round(Math.random * 10),
+      name: "Eoin Carrick",
+      school: "Maritime University",
+      certificate: true,
+    },
+    {
+      id: Math.round(Math.random * 10),
+      name: "Rocky Essel",
+      school: "U University",
+      certificate: false,
+    },
+    {
+      id: Math.round(Math.random * 10),
+      name: "Weddey Akison",
+      school: "A University",
+      certificate: true,
+    },
+    {
+      id: Math.round(Math.random * 10),
+      name: "Saint John",
+      school: "X University",
+      certificate: true,
+    },
+    {
+      id: Math.round(Math.random * 10),
+      name: "John Doe",
+      school: "C University",
+      certificate: false,
+    },
+    {
+      id: Math.round(Math.random * 10),
+      name: "Kofi Tetteh",
+      school: "F University",
+      certificate: true,
+    },
+  ]);
+  return <div className="App">
+    <p>Hello Wolds</p>
+  </div>;
 };
 
 export default App;
---
+++ reactapp/src/components/index.js
@@ -0,0 +1,6 @@
+const name ={
+    firstName: 'Eoin',
+    lastName: 'Carrick'
+}
+
+export default name
