5d2d7b108d4765fdca6153242a3ac2bec682ca8b
Eoin Carrick <22carri99@gmail.com>
2022-02-20 11:05:22 +0000

    code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,8 +1,26 @@
 import "./css/App.css";
+import React from "react";
+import Home from "./components/pages/Home";
+import Demo from "./components/pages/Demo";
+import KeyFeatures from "./components/pages/KeyFeatures";
+import Pricing from "./components/pages/Pricing";
+import Testimonials from "./components/pages/Testimonials";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="App">
-      <p>Hello Wolds</p>
+    <div className="container">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/features" element={<KeyFeatures />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/demo" element={<Demo />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
---
+++ reactapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { MenuList } from "./MenuList";
+
+const Navbar = () => {
+  const menuList = MenuList.map(({ url, title }, index) => {
+    return (
+      <li key={index}>
+        <a href={url}>{title}</a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <div className="logo">
+        VPN<font>Lab</font>
+      </div>
+      <ul className="menu0list">{menuList}</ul>
+    </nav>
+  );
+};
+
+export default Navbar;
