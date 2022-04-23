a32a42ecbcdbe6230fcfd7e81690f45790d1d1a8
Eoin Carrick <22carri99@gmail.com>
2022-04-23 21:32:05 +0000

    reac

--- react-ip/src/App.js
+++ react-ip/src/App.js
@@ -1,10 +1,12 @@
-import './styles/App.css';
+import React from 'react';
 import { Navbar } from './components';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Home />
     </div>
   );
 }
--- react-ip/src/components/Navbar.jsx
+++ react-ip/src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../styles/index.css';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
-}
+    <nav className='navbar'>
+      <h1>The Dojo Blog</h1>
+      <div className='links'>
+        <a href='/'>Home</a>
+        <a
+          href='/create'
+          style={{
+            color: 'white',
+            backgroundColor: '#f1356d',
+            borderRadius: '8px',
+          }}
+        >
+          New Blog
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
